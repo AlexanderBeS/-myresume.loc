a6fc4d55243487919cc27844ef89a337ea77cfa9
AlexanderBeS <oibespalov@gmail.com>
2020-06-03 09:02:50 +0300

    added priveleges template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,12 +9,17 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="icon" href="{!! asset('images/resume.png') !!}"/>
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" rel="stylesheet">
+    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/js/all.min.js"></script>--}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -26,106 +31,20 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
-<style>
-    .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-    }
-
-    @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-            font-size: 3.5rem;
-        }
-    }
-
-    html, body {
-        height: 100% !important;
-    }
-    body {
-        display: flex!important;
-        flex-direction: column!important;
-    }
-    .content {
-        flex: 1 0 auto!important;
-    }
-</style>
-
 </head>
 <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown01">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-            </ul>
-
-        </div>
-        <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
-            @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                </li>
-                @if (Route::has('register'))
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                    </li>
-                @endif
-            @else
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('resumes.index') }}">Мои резюме</a>
-                        <a class="dropdown-item" href="{{ route('resumes.create') }}">Создать резюме</a>
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                           document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-            @endguest
-        </ul>
-    </nav>
-    <main class="py-4 content w-100 container">
-        @yield('content')
-    </main>
 
 
-    @include('layouts.footer')
+@include('layouts.navbar')
+
+@include('layouts.priveleges')
+
+<main class="py-4 content w-100 container">
+    @yield('content')
+</main>
+
+
+@include('layouts.footer')
 
 </body>
 </html>
---
+++ resources/views/layouts/priveleges.php
@@ -0,0 +1,150 @@
+<div id="headerwrap">
+    <div class="container">
+        <div class="row centered">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h1>It Doesn't Take a Rocket <b>Scientist</b></h1>
+                <h2>It Takes a Designer</h2>
+            </div>
+        </div>
+        <!-- row -->
+    </div>
+    <!-- container -->
+</div>
+
+
+<div class="container w">
+    <div class="row centered">
+        <br><br>
+        <div class="col-lg-4">
+            <i class="fa fa-heart"></i>
+            <h4>DESIGN</h4>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even believable.</p>
+        </div>
+        <!-- col-lg-4 -->
+
+        <div class="col-lg-4">
+            <i class="fa fa-laptop"></i>
+            <h4>BOOTSTRAP</h4>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even believable.</p>
+        </div>
+        <!-- col-lg-4 -->
+
+        <div class="col-lg-4">
+            <i class="fa fa-trophy"></i>
+            <h4>SUPPORT</h4>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even believable.</p>
+        </div>
+        <!-- col-lg-4 -->
+    </div>
+    <!-- row -->
+    <br>
+    <br>
+</div>
+
+
+<div id="lg">
+    <div class="container">
+        <div class="row centered">
+            <h4>OUR AWESOME CLIENTS</h4>
+            <div class="item-logo">
+                <div class="item-logo-img work1"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work2"></div>
+            </div>
+            <!--
+            <div class="item-logo">
+                <div class="item-logo-img work3"></div>
+            </div>
+            -->
+            <div class="item-logo">
+                <div class="item-logo-img work4"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work5"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work6"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work7"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work8"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work9"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work10"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work11"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work12"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work13"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work14"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work15"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work16"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work17"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work18"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work19"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work20"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work21"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work22"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work23"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work24"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work25"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work26"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work27"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work28"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work29"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work30"></div>
+            </div>
+            <div class="item-logo">
+                <div class="item-logo-img work31"></div>
+            </div>
+
+
+        </div>
+        <!-- row -->
+    </div>
+    <!-- container -->
+</div>
